f8cc1e704c0b1b60301310d612898d1a66c03087
EliDev <oscarmateoelias@gmail.com>
2024-11-02 16:27:54 -0600

    Add Cors Config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import {connectDB} from "../config/db";
+import cors from 'cors';
+import {corsConfig} from "./config/cors";
+import {connectDB} from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
 
 // Load environment variables
@@ -10,6 +12,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors(corsConfig ))
 
 // Enable read express.json
 app.use(express.json());
